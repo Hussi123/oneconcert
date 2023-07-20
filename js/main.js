c597fa6eee6188a6d366328c244de6264012c1c4
Husniddin Abdullayev <132268896+Hussi123@users.noreply.github.com>
2023-07-20 15:44:28 +0500

    responsive fineshed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,73 @@
-let swiper = new Swiper("mySwiper", {
+// Initialize Swiper
+let swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    grabCursor: true,//cursor
     slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 3,
+    spaceBetween: 30,
     loop: true,
-    loopFillGroupWithBlank: true,
+    autoplay: true,
+    freeMode: false,//??????
+    mousewheel: false,//o'tkazish effektlari
+
+    keyboard: {
+        enabled: true,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        type: "fraction",
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".next__btn",
+        prevEl: ".prev__btn",
     },
+    breakpoints: {
+        300: {
+            centeredSlides: false,
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+       
+        768: {
+            centeredSlides: true,
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1100: {
+            centeredSlides: true,
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+
+});
+
+
+const swipers = new Swiper(".swiper-slider", {
+    // Optional parameters
+    keyboard: {
+        enabled: true
+    },
+
+    // Enabled autoplay mode
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: false,
+        clickable: true
+    },
+
+    // If we need navigation
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    // Responsive breakpoints
+
 });
